89a7c1649f1a9c0f0ed230d1d50f7e687fc05d6d
sedatsamet <46195164+sedatsamet@users.noreply.github.com>
2022-09-21 15:19:35 +0300

    creating variables project

---
+++ invioOdev1.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+// Variables
+
+// islem table variables
+var islemNo : Int = 102
+var ogrNo : Int = 422
+var kitapNo : Int = 23
+var aTarih : String = "20/09/2022"
+var vTarih : String = "21/09/2022"
+
+// ogrenci table variables
+var ogrAd : String = "Sedat Samet"
+var ogrSoyad : String = "Oypan"
+var cinsiyet : String = "Erkek"
+var dTarih : String = "01/08/1998"
+var sinif : String = "A-101"
+
+// kitap table variables
+var isbnNo : Int = 308874681
+var kitapAdi : String = "And Then There Were None"
+var yazarNo : Int = 742
+var turNo : Int = 5
+var sayfaSayisi : Int = 272
+var puan : Int = 8
+
+// yazar table variables
+var yazarAd : String = "Agatha"
+var yazarSoyad : String = "Christie"
+
+// tur table variables
+var turAdi : String = "Crime"
